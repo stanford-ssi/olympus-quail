580dd3e7207ce772706e8487feede60ba1d7cf39
Ahmed <aiabdalla5050@gmail.com>
2019-07-31 23:41:39 -0700

    presenting...penguin

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,12 @@
 #undef min
 #undef max
 #include <pin_interface.h>
-#include <solenoids.hpp>
-#include <pressure_sensor.hpp>
 #include "SD.h"
-#include "MC33797.h"
-#include <SPI.h>
 #include "wiring_private.h"
 #include <iostream.>
 #include <fstream.>
+#include <cstdint>
+using namespace std;
 //#include <datalogger.hpp>
 
 File dataFile;
@@ -27,7 +25,7 @@ String inData;
 String lastCommand = "00";
 int command;
 
-const string logFileName = "ignition.txt"; // Must be 8 chars or less
+const char logFileName[] = "ignition.txt"; // Must be 8 chars or less
 ofstream output;
 
 //Creates Packets to send to Print and Log functions
@@ -52,33 +50,30 @@ void setup() {
   pinMode(SD_Card_SS, OUTPUT);
   digitalWrite(LED_D2, HIGH);
   digitalWrite(LED_D3, LOW);
-  output.open(logFileName);
-  if(!output.is_open()) {
-    Serial.println("file not found");
-  }
-  // see if the card is present and can be initialized:
-  if (!SD.begin(SD_Card_SS)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1) ;
-  }
+  //TODO: uncomment before test
+  // // see if the card is present and can be initialized:
+  // if (!SD.begin(SD_Card_SS)) {
+  //   Serial.println("Card failed, or not present");
+  //   // don't do anything more:
+  //   while (1) ;
+  // }
 
   digitalWrite(LED_D2, HIGH);
   digitalWrite(LED_D3, LOW);
 
-  //Open up the file we're going to log to!
-  // dataFile = SD.open(logFileName, ((O_RDWR|O_APPEND)));
-  // if (!dataFile) {
-  //   Serial.println("error opening datalog.txt");
-  //   // Wait forever since we cant write data
-  //    while (!dataFile) {
-  //       Serial.println("error opening datalog.txt");
-  //       delay(500);
-  //       dataFile = SD.open(logFileName, (O_RDWR|O_APPEND));
-
-  //    }
-  // }
-  // dataFile.println("Data logging for Quail has begun yeet");
+  //TODO: uncomment before test
+  // // Open up the file we're going to log to!
+  dataFile = SD.open(logFileName, ((O_RDWR|O_APPEND)));
+  if (!dataFile) {
+    Serial.println("error opening ignition.txt");
+    // Wait forever since we cant write data
+     while (!dataFile) {
+        Serial.println("error opening ignition.txt");
+        delay(500);
+        dataFile = SD.open(logFileName, (O_RDWR|O_APPEND));
+     }
+  }
+  dataFile.println("Data logging for Quail has begun yeet");
   Serial.println("Setup done");
 }
 
@@ -92,12 +87,12 @@ void loop() {
     unsigned long loadCell = analogRead(Analog_Sensor_In);
     data = createDataPacket(currTime, loadCell, lastCommand);
 
-    //dataFile.println(data);
+    //TODO: uncomment 
     //make sure to uncomment this before testing
     if ((currTime - 5) > startTime) {
       startTime = currTime;
+      dataFile.println(data);
       Serial.println(data + "");
-      output << data + '\n';
     }
   }
 } // end of loop
@@ -124,7 +119,7 @@ String createDataPacket(unsigned long timeMillis, unsigned long loadCell, String
   String out = "0, ";
 
   for (int i = 0; i < 5; i++) {
-    out += "0, ";
+    out += "-1, ";
   }
   out = out + loadCell + ",";
   out = out + timeMillis + ",";
